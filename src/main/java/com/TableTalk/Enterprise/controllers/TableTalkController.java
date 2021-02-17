e8463a971e3e90ab6051e2bc22e893f5ec927483
Storm Hamilton <hamilsu@mail.uc.edu>
2021-02-17 18:36:27 -0500

    Added some basic DTO, including a relationship between User and ProfilePicture

--- src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
+++ src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
@@ -1,6 +1,7 @@
 package com.tabletalk.enterprise.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
     @Controller
