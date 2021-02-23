cb0d18044fb540d7e971c9c3d4974328809cc2dc
greeleywebdev <54598615+greeleywebdev@users.noreply.github.com>
2021-02-23 17:42:24 -0500

    beginning of home page

--- src/main/java/com/TableTalk/Enterprise/EnterpriseApplication.java
+++ src/main/java/com/TableTalk/Enterprise/EnterpriseApplication.java
@@ -1,13 +1,27 @@
-package com.tabletalk.enterprise;
+package com.TableTalk.Enterprise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class EnterpriseApplication {
+public class
+EnterpriseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EnterpriseApplication.class, args);
+	}
+
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/**")
+					.addResourceLocations("classpath:/assets/", "classpath:/styles/")
+					.setCachePeriod(0);
+		}
 	}
 
 }
