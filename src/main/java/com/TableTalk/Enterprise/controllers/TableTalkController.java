d487ebf5e8aa0ca0484c0867fffc26ab5ef9390b
mkone25 <konemuc@gmail.com>
2021-02-28 23:37:32 -0500

    removed what was added on master

--- src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
+++ src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
@@ -1,22 +1,23 @@
 package com.tabletalk.enterprise.controllers;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-    public class TableTalkController {
-        /**
-         * Handle the root (/) endpoint and return a start page.
-         * @return
-         */
-        @RequestMapping("/")
-        public String index() {
-            return "start";
-        }
+public class TableTalkController {
 
+    /**
+     * Handle the root (/) endpoint and return a start page.
+     * @return
+     */
+
+
+    @RequestMapping("/")
+    public String index() {
+        return "start";
     }
+}
 
 
 
