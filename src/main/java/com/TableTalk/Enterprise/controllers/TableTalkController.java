8a96e45325c5ec58c9afa8544535a6eb5aa5e9fe
mkone25 <konemuc@gmail.com>
2021-02-28 23:31:52 -0500

    removed what was added on master

--- src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
+++ src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
@@ -16,58 +16,6 @@ import org.springframework.web.bind.annotation.*;
             return "start";
         }
 
-        @GetMapping("/Game")
-        public ResponseEntity fetchGames(){
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        @PostMapping(value="/Game", consumes="application/jason", produces="application/json")
-        public com.tabletalk.enterprise.dto.Game createGame(@RequestBody com.tabletalk.enterprise.dto.Game game){
-            return game;
-        }
-        @DeleteMapping("/Game")
-        public ResponseEntity deleteGames(){
-            return new ResponseEntity(HttpStatus.OK);
-        }
-
-        @GetMapping("/ProfilePicture")
-        public ResponseEntity fetchProfilePicture(){
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        @PostMapping(value="/ProfilePicture", consumes="application/jason", produces="application/json")
-        public com.tabletalk.enterprise.dto.ProfilePicture createProfilePicture(@RequestBody com.tabletalk.enterprise.dto.ProfilePicture profilePicture){
-            return profilePicture;
-        }
-        @DeleteMapping("/ProfilePicture")
-        public ResponseEntity deleteProfilePicture(){
-        return new ResponseEntity(HttpStatus.OK);
-        }
-
-        @GetMapping("/Room")
-        public ResponseEntity fetchRooms(){
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        @PostMapping(value="/Room", consumes="application/jason", produces="application/json")
-        public com.tabletalk.enterprise.dto.Room createRoom(@RequestBody com.tabletalk.enterprise.dto.Room room){
-            return room;
-        }
-        @DeleteMapping("/Room")
-        public ResponseEntity deleteRoom(){
-        return new ResponseEntity(HttpStatus.OK);
-        }
-
-        @GetMapping("/User")
-        public ResponseEntity fetchUsers(){
-            return new ResponseEntity(HttpStatus.OK);
-        }
-        @PostMapping(value="/User", consumes="application/jason", produces="application/json")
-        public com.tabletalk.enterprise.dto.User createUser(@RequestBody com.tabletalk.enterprise.dto.User user){
-            return user;
-        }
-        @DeleteMapping("/User")
-        public ResponseEntity deleteUser(){
-        return new ResponseEntity(HttpStatus.OK);
-        }
-
     }
 
 
