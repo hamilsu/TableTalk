c5835c1108998dd9d126b3f44c95ee37b7e544b6
vmanchaiah <66232756+vmanchaiah@users.noreply.github.com>
2021-03-02 19:38:28 -0500

    Added JavaDoc for Room class

--- src/main/java/com/TableTalk/Enterprise/dto/Room.java
+++ src/main/java/com/TableTalk/Enterprise/dto/Room.java
@@ -5,6 +5,13 @@ import lombok.Data;
 import java.time.LocalDateTime;
 import java.util.List;
 
+/** Main class that controls the collaboration aspects of each game night. 
+* Allows for invite links to be generated, the list of games available in the host’s library, 
+* and a description of the of game night the host wants to plan. 
+* @param gameID the unique identifier for the game that you are a part of
+* @return game the game that you will be playing
+*/
+
 public @Data class Room {
     private String gameId;
     private List<User> listOfPlayers;
