613fbbeb0ac8a0629f74fd5f181d0623d5344d50
vmanchaiah <66232756+vmanchaiah@users.noreply.github.com>
2021-03-02 19:25:12 -0500

    Updated variable name

--- src/main/java/com/TableTalk/Enterprise/dto/Room.java
+++ src/main/java/com/TableTalk/Enterprise/dto/Room.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public @Data class Room {
-    private String id;
+    private String gameId;
     private List<User> listOfPlayers;
     private LocalDateTime finalizedDate; //Might opt to change this to a nullable class since initially dates would be tbd
     private String address;
