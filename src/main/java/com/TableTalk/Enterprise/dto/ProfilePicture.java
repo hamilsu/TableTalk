c762c8be64e8c4b3fb69714b07e3af17533d6945
Benjamin Romig <romigbl@mail.uc.edu>
2021-03-04 12:39:10 -0500

    Removed unnecessary or unused files and comments

--- src/main/java/com/TableTalk/Enterprise/dto/ProfilePicture.java
+++ src/main/java/com/TableTalk/Enterprise/dto/ProfilePicture.java
@@ -4,20 +4,13 @@ import lombok.Data;
 
 //creating a profile picture class for efficiency. Might change it to a generic Photo class if we need photos anywhere else.
 
-   // @Entity
-   // @Table(name="photos")
     public @Data
     class ProfilePicture {
 
-       // @Id
-      //  @GeneratedValue
         private int photoId;
         private String path;
         private String fileName;
 
-     //   @ToString.Exclude
-      //  @ManyToOne
-      //  @JoinColumn(name="username")
         private com.TableTalk.Enterprise.dto.User user;
     }
 
