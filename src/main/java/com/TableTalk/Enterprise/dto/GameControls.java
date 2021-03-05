5d576ab754881e9dc1062c6fcb06881445a6a935
vmanchaiah <66232756+vmanchaiah@users.noreply.github.com>
2021-03-04 21:26:54 -0500

    Fixing variable names

--- src/main/java/com/TableTalk/Enterprise/dto/GameControls.java
+++ src/main/java/com/TableTalk/Enterprise/dto/GameControls.java
@@ -3,8 +3,8 @@ package com.tabletalk.enterprise.dto;
 import lombok.Data;
 
 public @Data class GameControls {
-    private int min_Players;
-    private int max_Players;
-    private int min_gameTime;
-    private int max_gameTime;
+    private int minPlayers;
+    private int maxPlayers;
+    private int mingameTime;
+    private int maxgameTime;
 }
