2c286cc22009896eaaf5e96ab3944f4fd43f9172
maxedout34 <17033376+maxedout34@users.noreply.github.com>
2021-04-06 19:52:33 -0400

    Code Review 2
    Code review 2

--- src/main/java/com/TableTalk/Enterprise/dao/IRoomDAO.java
+++ src/main/java/com/TableTalk/Enterprise/dao/IRoomDAO.java
@@ -6,11 +6,34 @@ import java.io.IOException;
 import java.util.List;
 
 public interface IRoomDAO {
+    /**
+     * Generate a room
+     *
+     * @param room
+     * @return
+     * @throws Exception
+     */
     Room save (Room room) throws Exception;
 
+    /**
+     * Fetch all rooms
+     *
+     * @return
+     */
     List<Room> fetchAll();
 
+    /**
+     * Search for room by ID
+     *
+     * @param id
+     * @return
+     */
     Room fetch(int id);
 
+    /**
+     * Delete Room
+     *
+     * @param id
+     */
     void delete(int id);
 }
