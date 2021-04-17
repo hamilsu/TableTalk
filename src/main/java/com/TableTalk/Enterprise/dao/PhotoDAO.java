3e4b998d3dc5d242b4207be0bfe910cbe7a8dd28
Anne Catherwood <annecatherwood@gmail.com>
2021-04-16 23:30:28 -0400

    find or create photo directory

--- src/main/java/com/TableTalk/Enterprise/dao/PhotoDAO.java
+++ src/main/java/com/TableTalk/Enterprise/dao/PhotoDAO.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -24,12 +25,22 @@ public class PhotoDAO implements IPhotoDAO {
     @Override
     public void saveImage(MultipartFile imageFile, Photo photo) throws IOException {
         Path currentPath = Paths.get("");
-        Path absolutePath = currentPath.toAbsolutePath();
-        photo.setPath(absolutePath + "/src/main/upload/" + imageFile.getOriginalFilename());
+        Path absolutePath = Paths.get(currentPath.toAbsolutePath() + "/src/main/upload/");
+        System.out.println(absolutePath);
+        if (!Files.exists(absolutePath)){
+            Files.createDirectory(absolutePath);
+            //TODO: logging
+            System.out.println("Directory created");
+        }else{
+            //TODO: logging
+            System.out.println("Directory already exists");
+        }
+
+        photo.setPath(absolutePath + "/" + imageFile.getOriginalFilename());
         byte[] bytes = imageFile.getBytes();
         Path path = Paths.get(photo.getPath());
-        System.out.println(path);
         Files.write(path, bytes);
+
     }
 }
 
