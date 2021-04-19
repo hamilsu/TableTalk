115c2f96f9e67d086913d816f528d96d3534f3c1
Storm Hamilton <hamilsu@mail.uc.edu>
2021-04-19 17:43:52 -0400

    Trying something with the config.yml to forcibly set the profile as test in CICD

--- src/main/java/com/TableTalk/Enterprise/dao/RoomDAOStub.java
+++ src/main/java/com/TableTalk/Enterprise/dao/RoomDAOStub.java
@@ -1,6 +1,7 @@
 package com.TableTalk.Enterprise.dao;
 
 import com.TableTalk.Enterprise.dto.Room;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 import java.util.ArrayList;
