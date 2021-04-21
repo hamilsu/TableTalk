df5ecc6036243e03d9d4b90f2b715e962a2d555d
Anne Catherwood <annecatherwood@gmail.com>
2021-04-21 16:17:21 -0400

    code clean up

--- src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
+++ src/main/java/com/TableTalk/Enterprise/controllers/TableTalkController.java
@@ -325,43 +325,6 @@ public class TableTalkController {
         modelAndView.addObject("room", room);
         modelAndView.setViewName("room");
         return modelAndView;
-//        Photo photo = new Photo();
-//        try {
-//            roomService.save(room);
-//
-//            if (!imageFile.isEmpty()){
-//                try {
-//                    photo.setFileName(imageFile.getOriginalFilename());
-//                    photo.setRoom(room);
-//                    roomService.saveImage(imageFile, photo);
-//                    photo.setPath("/src/main/upload/" + photo.getFileName());
-//                    room.photos.add(photo);
-//                } catch (IOException e) {
-//                    modelAndView.setViewName("error");
-//                    return modelAndView;
-//                }
-//            }
-//
-//
-//
-//            List<Photo> photos = new ArrayList<Photo>();
-//            photos = room.getPhotos();
-//            model.addAttribute("room", room);
-//            model.addAttribute("photos", photos);
-////
-////            modelAndView.addObject("photos", photos);
-//            modelAndView.setViewName("room");
-//        }catch (Exception e) {
-//            e.printStackTrace();
-//            modelAndView.setViewName("error");
-//            return  modelAndView;
-//        }
-//
-//
-//        modelAndView.addObject("game", game);
-//        modelAndView.addObject("photos", photo);
-//        modelAndView.addObject("room", room);
-//        return modelAndView;
     }
 
 
